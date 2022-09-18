57adb2b1afd4de7aea1aa714ab7776cd37fdc05f
alex_m_s <alex_m_s@yahoo.com>
2022-09-18 12:34:20 +0200

    added user model

--- coworking-space/src/main/java/io/AlMaSm7/coworkingspace/model/User.java
+++ coworking-space/src/main/java/io/AlMaSm7/coworkingspace/model/User.java
@@ -1,2 +1,36 @@
-package io.AlMaSm7.coworkingspace.model;public class User {
+package io.AlMaSm7.coworkingspace.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    private long id;
+    @Getter
+    @Setter
+    private String firstname;
+    @Getter
+    @Setter
+    private String lastname;
+    @Getter
+    @Setter
+    private String email;
+    @Getter
+    @Setter
+    private String password;
+    @Getter
+    @Setter
+    private int role;
+
 }
